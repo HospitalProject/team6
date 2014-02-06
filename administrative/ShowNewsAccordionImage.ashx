f53d7eb575da6158889909ebec12036034ed104e
ben-mo <ben.q.mo@gmail.com>
2014-02-05 21:51:59 -0500

    Ben - Add the first init of news accordion feature

---
+++ administrative/ShowNewsAccordionImage.ashx
@@ -0,0 +1,39 @@
+﻿<%@ WebHandler Language="C#" Class="ShowNewsAccordionImage" %>
+
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public class ShowNewsAccordionImage : IHttpHandler {
+    
+    public void ProcessRequest (HttpContext context) {
+        int _an_id = Convert.ToInt32(context.Request.QueryString["an_id"]);
+        context.Response.ContentType = "image/jpeg";
+        Stream strm = ShowImage(_an_id);
+        byte[] buffer = new byte[4096];
+        int byteSeq = strm.Read(buffer, 0, 4096);
+        while (byteSeq > 0)
+        {
+            context.Response.OutputStream.Write(buffer, 0, byteSeq);
+            byteSeq = strm.Read(buffer, 0, 4096);
+        }  
+    }
+ 
+    public Stream ShowImage(int _id)
+    {
+        linqNewsAccordion objNewsAccordion = new linqNewsAccordion();
+
+        return new MemoryStream(objNewsAccordion.getNewsAccordionByID(_id).First().an_img.ToArray());
+    }
+    
+    public bool IsReusable {
+        get {
+            return false;
+        }
+    }
+
+}
---
+++ news_accordion.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class news_accordion : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string strNewsAccordionContent = string.Empty;
+        string strAnTitle = string.Empty;
+        string strAnContent = string.Empty;
+        linqNewsAccordion objAccordionNews = new linqNewsAccordion();
+
+        var objRecentNews = objAccordionNews.getRecentNewsAccordion();
+        foreach (var objNews in objRecentNews)
+        {
+            strAnTitle = objNews.an_title.ToString();
+            strAnContent = objNews.an_content.ToString();
+            strNewsAccordionContent += "<h3>" + strAnTitle.Substring(0, strAnTitle.Length > 15 ? 15 : strAnTitle.Length) + (strAnTitle.Length > 15 ? "..." : "") + "</h3>";
+            strNewsAccordionContent += "<div>" + strAnContent.Substring(0, strAnContent.Length > 200 ? 200 : strAnContent.Length) + (strAnContent.Length > 200 ? "..." : "") + "</div>";
+        }
+
+        ltl_news_accordion.Text = strNewsAccordionContent;
+    }
+}
