89d1bbb396eb2d0924226b617c75dd65ed798682
ben-mo <ben.q.mo@gmail.com>
2014-02-22 18:28:02 -0500

    Ben - Fix a incorrect link in the accordion news editing page that causes the image can't be shown while editing

--- administrative/admin_news_accordion.aspx.cs
+++ administrative/admin_news_accordion.aspx.cs
@@ -112,7 +112,7 @@ public partial class admin_news_accordion : System.Web.UI.Page
         }
         else
         {
-            ((Image)(fmv_news.FindControl("img_newsImageE"))).ImageUrl = "ShowNewsAccordionImage.ashx?an_id=" + _id.ToString();
+            ((Image)(fmv_news.FindControl("img_newsImageE"))).ImageUrl = "../ShowNewsAccordionImage.ashx?an_id=" + _id.ToString();
         }
 
     }
