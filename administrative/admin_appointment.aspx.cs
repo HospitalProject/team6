f52f0f063d5d4211ae61f4af933fc8736b23a962
ben-mo <ben.q.mo@gmail.com>
2014-04-02 22:54:57 -0400

    Ben - appointment feature done without styling

--- administrative/admin_appointment.aspx.cs
+++ administrative/admin_appointment.aspx.cs
@@ -11,18 +11,36 @@ public partial class admin_appointment : System.Web.UI.Page
     {
         if (!Page.IsPostBack)
         {
-            _subRebind();
+            _subRebindDocList();
             _showPanel(pnl_doctorList);
         }
     }
 
-    private void _subRebind()
+    private void _subRebindDocList()
     {
         linqUsers objUsers = new linqUsers();
 
         var allDoctors = objUsers.getAllDoctors();
         rpt_doctorlist.DataSource = allDoctors;
         rpt_doctorlist.DataBind();
+    }
+
+
+    private void _subRebindDocTimeslot()
+    {
+        Guid _doctor_id = new Guid(ViewState["doctor_id"].ToString());
+        linqUsers objUser = new linqUsers();
+        ddl_docavailable_date.DataSource = objUser.getDistinctActiveAvailableDateByDocID(_doctor_id);
+        ddl_docavailable_date.DataTextField = "available_date";
+        ddl_docavailable_date.DataValueField = "available_date";
+        ddl_docavailable_date.DataBind();
+        rpt_doctimeslotlist.DataSource = null;
+        rpt_doctimeslotlist.DataBind();
+        if (ddl_docavailable_date.Items.Count != 0)
+        {
+            rpt_doctimeslotlist.DataSource = objUser.getAppOfDocCertainDay(_doctor_id, Convert.ToDateTime(ddl_docavailable_date.SelectedItem.Value));
+            rpt_doctimeslotlist.DataBind();
+        }
     }
 
     private void _showPanel(Panel pnl)
@@ -40,17 +58,32 @@ public partial class admin_appointment : System.Web.UI.Page
         Guid _doctor_id;
         _doctor_id = new Guid(e.CommandArgument.ToString());
         ViewState["doctor_id"] = e.CommandArgument.ToString();
+        
         switch (e.CommandName)
         {
             case "ViewAppointment":
+                lbl_docappointment.Text = "Appointments for DR. " + getUserFullName(_doctor_id);
+                _subRebindDocAppointment();
                 _showPanel(pnl_appointments);
                 break;
             case "ViewTimeslot":
                 _showPanel(pnl_timeslot);
                 lbl_doctimeslot.Text = "Time slots for DR. " + getUserFullName(_doctor_id);
                 //lbl_doctimeslot.Text = e.CommandArgument.ToString();
+                _subRebindDocTimeslot();
                 break;
         }
+    }
+
+    private void _subRebindDocAppointment()
+    {
+        Guid _doctor_id = new Guid(ViewState["doctor_id"].ToString());
+        linqUsers objUser = new linqUsers();
+        rpt_activeappointmentlist.DataSource = objUser.getActiveAppointmentOfDoc(_doctor_id);
+        rpt_activeappointmentlist.DataBind();
+        rpt_archiveappointmentlist.DataSource = objUser.getArchivedAppointmentOfDoc(_doctor_id);
+        rpt_archiveappointmentlist.DataBind();
+
     }
 
     //Get user full name
@@ -78,15 +111,126 @@ public partial class admin_appointment : System.Web.UI.Page
         Guid _doctor_id = new Guid(ViewState["doctor_id"].ToString());
         lbl_docaddtimeslot.Text = "Add new available time slot for DR. " + getUserFullName(_doctor_id);
     }
+
+    //save new available time slots for a doctor
     protected void btn_savetimeslot_Click(object sender, EventArgs e)
     {
+        Guid _doctor_id = new Guid(ViewState["doctor_id"].ToString());
+        linqUsers objUser = new linqUsers();
+        TimeSpan _starttime = TimeSpan.Parse(ddl_starttime.SelectedItem.Value);
+        TimeSpan _endtime = TimeSpan.Parse(ddl_endtime.SelectedItem.Value);
+        DateTime _available_date = Convert.ToDateTime(txt_timeslotdate.Text);
+        TimeSpan i;
+        //add the available time slots as 30 minutes per slot
+        for (i = _starttime; i < _endtime; i = i + TimeSpan.FromMinutes(30))
+        {
+            objUser.insertAppointment(_doctor_id, _available_date, i, i + TimeSpan.FromMinutes(30));
+        }
+        //reset the input controls
+        txt_timeslotdate.Text = "";
+        ddl_starttime.SelectedIndex = ddl_starttime.Items.IndexOf(ddl_starttime.Items.FindByText("09:00"));
+        ddl_endtime.SelectedIndex = ddl_endtime.Items.IndexOf(ddl_endtime.Items.FindByText("09:30"));
+        _subRebindDocTimeslot();
         _showPanel(pnl_timeslot);
     }
+
+    //cancel saving time slots, back to previous view
     protected void btn_cancelsave_Click(object sender, EventArgs e)
     {
         txt_timeslotdate.Text = "";
         ddl_starttime.SelectedIndex = ddl_starttime.Items.IndexOf(ddl_starttime.Items.FindByText("09:00"));
         ddl_endtime.SelectedIndex = ddl_endtime.Items.IndexOf(ddl_endtime.Items.FindByText("09:30"));
         _showPanel(pnl_timeslot);
+    }
+    protected void ddl_docavailable_date_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        Guid _doctor_id;
+        _doctor_id = new Guid(ViewState["doctor_id"].ToString());
+        linqUsers objUser = new linqUsers();
+        if (ddl_docavailable_date.SelectedItem.Value != "")
+        {
+            rpt_doctimeslotlist.DataSource = objUser.getAppOfDocCertainDay(_doctor_id, Convert.ToDateTime(ddl_docavailable_date.SelectedItem.Value));
+            rpt_doctimeslotlist.DataBind();
+        }
+
+    }
+
+    //cancel appointment from doctor side
+    protected void lkb_doctimeslotcancelappointment_Command(object sender, CommandEventArgs e)
+    {
+        int _appointment_id = Int32.Parse(e.CommandArgument.ToString());
+        linqUsers objUser = new linqUsers();
+        objUser.cancelAppointment(_appointment_id);
+
+        Guid _doctor_id;
+        _doctor_id = new Guid(ViewState["doctor_id"].ToString());
+        if (ddl_docavailable_date.SelectedItem.Value != "")
+        {
+            rpt_doctimeslotlist.DataSource = objUser.getAppOfDocCertainDay(_doctor_id, Convert.ToDateTime(ddl_docavailable_date.SelectedItem.Value));
+            rpt_doctimeslotlist.DataBind();
+        }
+    }
+
+    //remove a timeslot
+    protected void lkb_removetimeslot_Command(object sender, CommandEventArgs e)
+    {
+        int _appointment_id = Int32.Parse(e.CommandArgument.ToString());
+        linqUsers objUser = new linqUsers();
+        objUser.removeTimeslot(_appointment_id);
+        Guid _doctor_id;
+        _doctor_id = new Guid(ViewState["doctor_id"].ToString());
+        if (ddl_docavailable_date.SelectedItem.Value != "")
+        {
+            rpt_doctimeslotlist.DataSource = objUser.getAppOfDocCertainDay(_doctor_id, Convert.ToDateTime(ddl_docavailable_date.SelectedItem.Value));
+            rpt_doctimeslotlist.DataBind();
+        }
+    }
+    protected void lkb_docappintmentcancelappointment_Command(object sender, CommandEventArgs e)
+    {
+        int _appointment_id = Int32.Parse(e.CommandArgument.ToString());
+        linqUsers objUser = new linqUsers();
+        objUser.cancelAppointment(_appointment_id);
+        _subRebindDocAppointment();
+    }
+    protected void ctv_endtime_ServerValidate(object source, ServerValidateEventArgs args)
+    {
+        if (TimeSpan.Parse(ddl_endtime.SelectedValue) > TimeSpan.Parse(ddl_starttime.SelectedValue))
+        {
+            args.IsValid = true;
+        }
+        else
+        {
+            args.IsValid = false;
+        }
+    }
+    protected void ctv_timeslotdate_ServerValidate(object source, ServerValidateEventArgs args)
+    {
+        try
+        {
+            if (DateTime.Parse(args.Value) >= DateTime.Now)
+            {
+                args.IsValid = true;
+            }
+            else
+            {
+                args.IsValid = false;
+            }
+        }
+        catch (Exception e)
+        {
+            args.IsValid = false;
+        }
+
+    }
+    protected void mnu_appointment_MenuItemClick(object sender, MenuEventArgs e)
+    {
+        if (e.Item.Text == "Archived")
+        {
+            mtv_docappointment.ActiveViewIndex = 1;
+        }
+        else
+        {
+            mtv_docappointment.ActiveViewIndex = 0;
+        }
     }
 }
--- patients/book_appointment.aspx.cs
+++ patients/book_appointment.aspx.cs
@@ -4,11 +4,187 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 public partial class book_appointment : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!Page.IsPostBack)
+        {
+            _subRebind();
+        }
+    }
 
+    private void _subRebind()
+    {
+        Guid _userId = (Guid)Membership.GetUser().ProviderUserKey;
+        linqUsers objUsers = new linqUsers();
+        
+        rpt_activeappointment.DataSource = objUsers.getAppointmentByPatientId(_userId, "active");
+        rpt_activeappointment.DataBind();
+        if (rpt_activeappointment.Items.Count == 0)
+        {
+            rpt_activeappointment.Visible = false;
+        }
+        else
+        {
+            rpt_activeappointment.Visible = true;
+        }
+        rpt_archivedappointment.DataSource = objUsers.getAppointmentByPatientId(_userId, "archived");
+        rpt_archivedappointment.DataBind();
+        if (rpt_archivedappointment.Items.Count == 0)
+        {
+            rpt_archivedappointment.Visible = false;
+        }
+        else
+        {
+            rpt_archivedappointment.Visible = true;
+        }
+    }
+
+    //get current user full name (First Name + Last Name)
+    public string getCurrentUserFullName()
+    {
+        //get current user id
+        Guid _userId = (Guid)Membership.GetUser().ProviderUserKey;
+
+        linqUsers objUsers = new linqUsers();
+        int count = objUsers.getUserAdditionalInfoByUserId(_userId).Count();
+
+        if (count == 0)
+            return "";
+        else
+        {
+            var objCurrentUser = objUsers.getUserAdditionalInfoByUserId(_userId).First();
+            return objCurrentUser.FirstName + " " + objCurrentUser.LastName;
+        }
+    }
+    protected void btn_makeappointment_Click(object sender, EventArgs e)
+    {
+        linqUsers objUser = new linqUsers();
+        var allDoctors = objUser.getAllDoctors();
+        ddl_doctorlist.Items.Clear();
+        ddl_availabledate.Items.Clear();
+        cbl_timeslot.Items.Clear();
+
+        //use this kind of code to bind the doctor/date/timeslot
+        //because doctor and timeslot are using multipul columns for the text field
+        //keep same style in this page
+        foreach (var doctor in allDoctors)
+        {
+            ddl_doctorlist.Items.Add(new ListItem(doctor.FirstName + " " + doctor.LastName, doctor.UserId.ToString()));
+        }
+
+        btn_submitAppointment.Enabled = false;
+
+        if (ddl_doctorlist.Items.Count > 0)
+        {
+            setAvailableDateList();
+        }
+
+        _showPanel(pnl_makeappointment);
+    }
+    protected void subCancelAppointment(object sender, CommandEventArgs e)
+    {
+        int _appointment_id = Int32.Parse(e.CommandArgument.ToString());
+        linqUsers objUser = new linqUsers();
+        objUser.cancelAppointment(_appointment_id);
+        _subRebind();
+    }
+
+    private void _showPanel(Panel pnl)
+    {
+        pnl_appointmentlist.Visible = false;
+        pnl_makeappointment.Visible = false;
+        pnl.Visible = true;
+    }
+
+    protected void ddl_doctorlist_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        setAvailableDateList();
+    }
+
+    private void setAvailableDateList()
+    {
+        linqUsers objUser = new linqUsers();
+        Guid _doctor_id = new Guid(ddl_doctorlist.SelectedValue);
+        var allDates = objUser.getDistinctActiveAvailableDateByDocID(_doctor_id);
+        ddl_availabledate.Items.Clear();
+        cbl_timeslot.Items.Clear();
+        foreach (var availabledate in allDates)
+        {
+            ddl_availabledate.Items.Add(new ListItem(availabledate.available_date, availabledate.available_date));
+        }
+
+        btn_submitAppointment.Enabled = false;
+
+        if (ddl_availabledate.Items.Count > 0)
+        {
+            setTimeSlotList();
+        }
+
+    }
+
+    private void setTimeSlotList()
+    {
+        cbl_timeslot.Items.Clear();
+        linqUsers objUser = new linqUsers();
+        Guid _doctor_id = new Guid(ddl_doctorlist.SelectedValue);
+        DateTime _available_date = DateTime.Parse(ddl_availabledate.SelectedValue);
+        var allTimeslots = objUser.getDoctorAvailableTimeslotOfDate(_doctor_id, _available_date);
+        foreach (var timeslot in allTimeslots)
+        {
+            cbl_timeslot.Items.Add(new ListItem(timeslot.available_starttime.ToString(@"hh\:mm") + " - " + timeslot.available_endtime.ToString(@"hh\:mm"), timeslot.appointment_id.ToString()));
+        }
+        if (cbl_timeslot.Items.Count > 0)
+        {
+            btn_submitAppointment.Enabled = true;
+        }
+        else
+        {
+            btn_submitAppointment.Enabled = false;
+        }
+
+    }
+    protected void ddl_availabledate_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        setTimeSlotList();
+    }
+    protected void btn_submitAppointment_Click(object sender, EventArgs e)
+    {
+        Guid _patient_id = (Guid)Membership.GetUser().ProviderUserKey;
+        linqUsers objUser = new linqUsers();
+        foreach (ListItem item in cbl_timeslot.Items)
+        {
+            if (item.Selected)
+            {
+                objUser.makeAppointment(_patient_id, Int32.Parse(item.Value));
+            }
+        }
+
+        ddl_doctorlist.Items.Clear();
+        ddl_availabledate.Items.Clear();
+        cbl_timeslot.Items.Clear();
+        _subRebind();
+        _showPanel(pnl_appointmentlist);
+    }
+    protected void btn_cancelSubmitAppointment_Click(object sender, EventArgs e)
+    {
+        ddl_doctorlist.Items.Clear();
+        ddl_availabledate.Items.Clear();
+        cbl_timeslot.Items.Clear();
+        _showPanel(pnl_appointmentlist);
+    }
+    protected void mnu_myappointment_MenuItemClick(object sender, MenuEventArgs e)
+    {
+        if (e.Item.Text == "Archived")
+        {
+            mtv_myappointment.ActiveViewIndex = 1;
+        }
+        else
+        {
+            mtv_myappointment.ActiveViewIndex = 0;
+        }
     }
 }
